578eafe7a7f2d2c41d40f7bddab1be9563a4278c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-27 18:11:48 -0700

    init project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Client folder
+app.use(express.static(__dirname + '/client'));
+
+// Body parser
+app.use(bodyParser.json());
+
+
+app.get('/', () => {
+    res.send('Please use /api/');
+});
+
+
+app.listen(port, () => {
+    console.log('Server started on Port ' + port);
+});
