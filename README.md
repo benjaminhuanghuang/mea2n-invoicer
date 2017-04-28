786218e88b14d122f5343bcd1c79168534b31d02
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-28 10:12:17 -0700

    add component and router

--- README.md
+++ README.md
@@ -6,5 +6,36 @@
     The client side project runs on port 4200, so we need enable CORS
     https://enable-cors.org
 
+## Run client side app
+    $ ng serve
+
 ## angular-cli create component
     $ ng g component navbar
+    angular cli will update app.module.ts to involve the new component automatically.
+
+## Router
+    add <base href="/"> in index.html
+    add Router in app.module.ts
+        import { RouterModule, Routes } from '@angular/router'
+        imports:[
+            RouterModule.forRoot(appRoutes),
+        ]
+        const appRoutes: Routes = [
+            {
+                path:'', component: CustomersComponent
+            }
+        ]   
+    Use router in app.component.html
+        <router-outlet></router-outlet>
+
+    Use router link in html
+        <li [routerLinkActive]="['active']" [routerLinkActiveOptions]="{exact:true}">
+            <a [routerLink]="['/']">Dashboard</a>
+        </li>
+
+## Bootstrap css can navbar
+    https://bootswatch.com -> Simplex ->download
+    Use the css in index.html
+
+    bootstrap -> Getting started -> Example -> Starter template -> view page source 
+    copy <nav>
--- invoicer-angular/src/app/app.module.ts
+++ invoicer-angular/src/app/app.module.ts
@@ -2,16 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { AddInvoiceComponent } from './components/add-invoice/add-invoice.component';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+
+const appRoutes: Routes = [
+  {
+    path:'', component: CustomersComponent
+  },
+  {
+    path:'customer/add', component: AddCustomerComponent
+  },
+  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddCustomerComponent,
+    AddInvoiceComponent,
+    CustomerDetailsComponent,
+    CustomersComponent,
+    EditCustomerComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
